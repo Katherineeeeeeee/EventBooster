be241a26805e7767914c9ebb4f3c8fec7cb041c8
Andrii <70772601+oLORDer@users.noreply.github.com>
2022-08-01 14:45:52 +0300

    Dev (#66)
    * base
    
    * base
    
    * base
    
    * base
    
    * logo
    
    * added fetch data
    
    * compleFooter
    
    * add modal
    
    * logo
    
    * add markup
    
    * createSprite
    
    * addaprive
    
    * addedToSprite
    
    * add style modal
    
    * finished dynamic base markup
    
    * update styles
    
    * pagin
    
    * header
    
    * add animation for buttons
    
    * added eventlisteners
    
    * comple header
    
    * comple header final
    
    * start modal window
    
    * pag
    
    * added event listener to serchQuery
    
    * added modal window to ticket
    
    * add theme switcher
    
    * pagin
    
    * fix
    
    * pag
    
    * add styles for btn
    
    * complete country selector
    
    * completer country search
    
    * theme
    
    * base
    
    * complete logic of modal window
    
    * svg
    
    * Update gallery.js
    
    * sss
    
    * complete icons
    
    * checkpoint
    
    * finished render-base-markup.js
    
    * theme
    
    * update header
    
    * styles for body
    
    * add styles + js modal
    
    * add
    
    * add modal styles
    
    * add scroll btn
    
    * complete country-select
    
    * fixed style for body
    
    * added Notiflix error and search for country
    
    * again
    
    * add
    
    * add modal changes
    
    * add modal js
    
    * complete buttona and footer hovers
    
    * paginal/theme
    
    * Rename Vector-light.svg to vector-light.svg
    
    * modal fix (#43)
    
    * Mp modal team (#44)
    
    * klnkl
    
    * 1746
    
    * ff
    
    * ff
    
    * add loader (#45)
    
    * fixed title (#46)
    
    * complete logic of event modal window (#47)
    
    * bugfixes (#48)
    
    * Modal card (#49)
    
    * modal fix
    
    * add
    
    * fix bugs for modal
    
    * remove console.log
    
    * complete hovers (#51)
    
    * vh
    
    * pag
    
    * Modal card (#52)
    
    * modal fix
    
    * add
    
    * fix bugs for modal
    
    * remove console.log
    
    * add scroll-bar styles
    
    * complete animation (#53)
    
    * complete hovers
    
    * complete animation
    
    * complete animation
    
    * bugfixes (#54)
    
    * checkpoint
    
    * bugfixes
    
    * map/pag/render (#55)
    
    Co-authored-by: oLORDer <justsendmeow@gmail.com>
    
    * add animation to modal (#56)
    
    * complete white theme and animation (#58)
    
    * complete hovers
    
    * complete animation
    
    * complete animation
    
    * complete white theme and animation
    
    * Favicon (#59)
    
    * add favicon
    
    * add
    
    * add
    
    * ffo
    
    * theme
    
    * pag
    
    * Change styles (#61)
    
    * add
    
    * mode Light - Dark by operation system
    
    * 2100 (#62)
    
    * ff
    
    * mm
    
    * mis
    
    * Dd finally (#65)
    
    * checkpoint
    
    * bugfixes
    
    * final push
    
    Co-authored-by: Katherineeeeeeee <katherine.dudar.7@gmail.com>
    Co-authored-by: oLORDer <justsendmeow@gmail.com>
    Co-authored-by: Denis Dashkevich <majorprestige99@gmail.com>
    Co-authored-by: zerkel1991 <acorus1991@gmail.com>
    Co-authored-by: Sergey Gulaga <ry6ens@gmail.com>
    Co-authored-by: Katherineeeeeeee <104833965+Katherineeeeeeee@users.noreply.github.com>
    Co-authored-by: Maksym Perlovskyi <103576383+Maksym150678@users.noreply.github.com>
    Co-authored-by: Denys Dashkevych <104511234+MajorPrestige@users.noreply.github.com>
    Co-authored-by: Vyacheslav <47851511+zerkel1991@users.noreply.github.com>

--- src/js/render-base-markup.js
+++ src/js/render-base-markup.js
@@ -28,9 +28,7 @@ async function onSerchQuerySubmit(e) {
 
   countryBefore = ticketmasterAPI.searchCountry;
   ticketmasterAPI.searchQuery = e.currentTarget.elements.serchQuery.value;
-
   ticketmasterAPI.page = 0;
-
   renderBaseMarkup();
 }
 
